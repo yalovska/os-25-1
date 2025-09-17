725e28d46729eb2be0625af1a2da85804af275ae
yalovska <115428375+yalovska@users.noreply.github.com>
2025-09-17 21:06:55 +0300

    Update and rename TODO.sh to variant1.sh

---
+++ lab1-shell-scripting/variant1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <directory> <filename>"
+    exit 1
+fi
+directory=$1
+filename=$2
+find "$directory" -type f -name "$filename.*" -exec dirname {} \; | sort
